1063e13018471e7d19af87430145e8ddf39230f0
Dustin Shaddawvine <104474633+Shaddawvine@users.noreply.github.com>
2022-07-08 13:28:58 -0600

    Add files via upload

---
+++ Script 2.sql
@@ -0,0 +1,8 @@
+Create table dndplayer(
+id int(10) not null auto_increment,
+first_name varchar(25) not null,
+last_name varchar(25) not null,
+race varchar(25) not null,
+class varchar(25) not null,
+primary key (id)
+);
